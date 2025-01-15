610a4709bc8cf886761747013075775e25a88d2f
duonganh203 <rankill52@gmail.com>
2025-01-15 16:03:02 +0700

    #11: setup msg commit

--- benkyo-server/src/index.ts
+++ benkyo-server/src/index.ts
@@ -7,5 +7,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:30001');
 });
