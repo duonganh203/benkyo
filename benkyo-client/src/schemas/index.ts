032e124eca8f78c9b016e2251499a29f1a4daa7a
duonganh203 <rankill52@gmail.com>
2025-01-28 11:28:31 +0700

    #21: implement login & register interface

--- benkyo-client/src/schemas/index.ts
+++ benkyo-client/src/schemas/index.ts
@@ -4,3 +4,8 @@ export const LoginSchema = z.object({
     email: z.string().email({ message: 'Please enter valid email address!' }),
     password: z.string().min(6, { message: 'Password must be at least 6 characters long!' })
 });
+
+export const RegisterSchema = LoginSchema.extend({
+    firstName: z.string().min(3, { message: 'Must be at least 3 characters long' }),
+    lastName: z.string().min(3, { message: 'Must be at least 3 characters long' })
+});
--- benkyo-client/src/types/api.ts
+++ benkyo-client/src/types/api.ts
@@ -1,4 +1,6 @@
 export interface ApiError {
     message: string;
     statusCode?: number;
+    errorCode: number;
+    errors: any;
 }
