2375b43a5f945f40a002ff24f002bdb94f522d78
duonganh203 <rankill52@gmail.com>
2025-01-28 09:55:28 +0700

    #21: add login feature & get me

--- benkyo-client/src/api/authApi.ts
+++ benkyo-client/src/api/authApi.ts
@@ -2,10 +2,15 @@ import { api } from '.';
 import { LoginPayload, RegisterPayload } from '../types/auth';
 
 export const login = async (payload: LoginPayload) => {
-    const { data } = await api.post('/login', payload);
+    const { data } = await api.post('auth/login', payload);
     return data;
 };
 
 export const register = async (payload: RegisterPayload) => {
-    await api.post('/register', payload);
+    await api.post('auth/register', payload);
+};
+
+export const me = async () => {
+    const { data } = await api.get('auth/me');
+    return data;
 };
