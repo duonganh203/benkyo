821859e2851908697863642d5d7cb685dc214581
duonganh203 <rankill52@gmail.com>
2025-01-28 09:55:28 +0700

    #21: add login feature & get me

--- benkyo-client/src/hooks/queries/useLogin.ts
+++ benkyo-client/src/hooks/queries/useLogin.ts
@@ -3,7 +3,6 @@ import { useMutation } from '@tanstack/react-query';
 import { login } from '@/api/authApi';
 import { ApiError } from '@/types/api';
 import { LoginPayload, User } from '@/types/auth';
-//options?: UseMutationOptions<{ user: User; token: string }, AxiosError<ApiError>, LoginPayload>
 const useLogin = () => {
     return useMutation<{ user: User; token: string }, AxiosError<ApiError>, LoginPayload>({
         mutationFn: login
