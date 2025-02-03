cdf5bba9be1c693b3d29f361f4b3e4e636156531
duonganh203 <rankill52@gmail.com>
2025-02-03 23:59:50 +0700

    #37: add basic hero section

--- benkyo-client/src/components/hero.tsx
+++ benkyo-client/src/components/hero.tsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { ArrowRightFromLineIcon } from 'lucide-react';
+
 const HeroSection = () => {
-    return <section>Hero section</section>;
+    return (
+        <section className='text-center pt-36 md:pt-48 pb-10'>
+            <div className='space-y-6 text-center'>
+                <div className='space-y-2 mx-auto'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl gradient-title'>
+                        Unlock Your Learning Potential with Benkyo <br />
+                        The Intelligent Flashcard App
+                    </h1>
+                    <p className='mx-auto max-w-[650px] text-muted-foreground md:text-xl'>
+                        Supercharge your learning with Benkyo. AI-driven flashcards personalize your study experience
+                        for maximum retention and success.
+                    </p>
+                </div>
+                <div>
+                    <Button size='lg' className='px-8' asChild>
+                        <Link to='/home'>
+                            Get Started <ArrowRightFromLineIcon />
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default HeroSection;
