d3b3d645e49ed29c67d859c802aaf11fbef2a154
duonganh203 <rankill52@gmail.com>
2025-03-23 21:53:05 +0700

    #65: refactor types

--- benkyo-client/src/types/card.ts
+++ benkyo-client/src/types/card.ts
@@ -17,3 +17,33 @@ export interface BatchImportCard {
     back: string;
     tags: string[];
 }
+export interface CardInterface {
+    _id: string;
+    deck: string;
+    front: string;
+    back: string;
+    tags: string[];
+    createdAt: string;
+    updatedAt: string;
+    media: CardMedia[];
+    learning: CardLearning;
+}
+export interface CardMedia {
+    type: 'image' | 'audio' | 'video';
+    url: string;
+    filename?: string;
+}
+
+export interface CardLearning {
+    state: State;
+    due: string;
+    elapsed_days: number;
+    stability: number;
+    difficulty: number;
+}
+export enum State {
+    NEW = 0,
+    LEARNING = 1,
+    REVIEW = 2,
+    RELEARNING = 3
+}
