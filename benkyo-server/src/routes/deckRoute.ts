e0664a2ffbb7c25f945a1804821181a293fa8669
duonganh203 <rankill52@gmail.com>
2025-03-24 01:06:09 +0700

    #75: compleate api for view deck

--- benkyo-server/src/routes/deckRoute.ts
+++ benkyo-server/src/routes/deckRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { getCards, getDeck } from '~/controllers/cardController';
-import { createDeck } from '~/controllers/deckController';
+import { createDeck, getAllDecks } from '~/controllers/deckController';
 import { errorHandler } from '~/errorHandler';
 import authMiddleware from '~/middlewares/authMiddleware';
 
 const deckRoutes: Router = Router();
 
 deckRoutes.post('/', [authMiddleware], errorHandler(createDeck));
+deckRoutes.get('/my-decks', [authMiddleware], errorHandler(getAllDecks));
 deckRoutes.get('/:id', [authMiddleware], errorHandler(getDeck));
 deckRoutes.get('/:id/cards', [authMiddleware], errorHandler(getCards));
 
