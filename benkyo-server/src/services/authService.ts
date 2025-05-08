8abfa88872bd1bb470a2c691c94fa9c21e51d5e1
duonganh203 <rankill52@gmail.com>
2025-05-09 01:39:49 +0700

    #167: add to lowercase email

--- benkyo-server/src/services/authService.ts
+++ benkyo-server/src/services/authService.ts
@@ -9,18 +9,18 @@ import * as jwt from 'jsonwebtoken';
 
 export const registerService = async (userData: z.infer<typeof registerValidation>) => {
     const { name, email, password } = userData;
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ email: email.toLowerCase() });
     if (user) {
         throw new BadRequestsException('User already exists!', ErrorCode.USER_ALREADY_EXISTS);
     }
     const hashedPassword = await hash(password, 10);
-    user = await User.create({ name, email, password: hashedPassword });
+    user = await User.create({ name, email: email.toLowerCase(), password: hashedPassword });
     return user;
 };
 
 export const loginService = async (userData: z.infer<typeof loginValidation>) => {
     const { email, password } = userData;
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email: email.toLowerCase() });
     if (!user) throw new BadRequestsException('Email or password is not correct!', ErrorCode.INVALID_CREDENTIALS);
     const isMatch = await compare(password, user.password);
     if (!isMatch) throw new BadRequestsException('Email or password is not correct!', ErrorCode.INVALID_CREDENTIALS);
