8cf98bfde623eb59692a745babb360ba8813c362
thienduyen12930 <144190277+thienduyen12930@users.noreply.github.com>
2025-06-24 23:09:35 +0700

    #184: view all accounts

--- benkyo-server/src/controllers/userController.ts
+++ benkyo-server/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { updateUserService } from '~/services/userService';
+import { updateUserService, listUserAccountsService } from '~/services/userService';
 import { updateUserValidation } from '~/validations/userValidation';
 
 export const updateUserProfile = async (req: Request, res: Response) => {
@@ -7,4 +7,8 @@ export const updateUserProfile = async (req: Request, res: Response) => {
     updateUserValidation.parse(userData);
     const updateUser = await updateUserService(req.user._id, userData);
     res.json(updateUser);
+};
+export const listUserAccounts = async (req: Request, res: Response) => {
+    const users = await listUserAccountsService();
+    res.json(users);
 };
--- benkyo-server/src/routes/userRoute.ts
+++ benkyo-server/src/routes/userRoute.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { updateUserProfile } from '~/controllers/userController';
+import { updateUserProfile, listUserAccounts } from '~/controllers/userController';
 
 import { errorHandler } from '~/errorHandler';
 import authMiddleware from '~/middlewares/authMiddleware';
-
+import adminAuthMiddleware from '~/middlewares/adminAuthMiddleware';
 const userRoutes: Router = Router();
 
 userRoutes.patch('/update', [authMiddleware], errorHandler(updateUserProfile));
-
+userRoutes.get('/listAccounts', [adminAuthMiddleware], errorHandler(listUserAccounts));
 export default userRoutes;
