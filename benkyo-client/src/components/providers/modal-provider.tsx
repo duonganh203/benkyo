64a7f677dfcca322c28e5e59f1b7447a50f3f582
tienhuuvo <tienvh08082003@gmail.com>
2025-06-28 09:59:16 +0700

    #187: fix streak icon

--- benkyo-client/src/components/providers/modal-provider.tsx
+++ benkyo-client/src/components/providers/modal-provider.tsx
@@ -1,6 +1,7 @@
 import { CreateDeckModal } from '../modals/create-deck-modal';
 import { GenerateQuizModal } from '../modals/generate-quiz-modal';
 import { RequestPublicDeckModal } from '../modals/request-public-deck-modal';
+import StreakIconContainer from './streak-icon-provider';
 
 export default function ModalProvider() {
     return (
@@ -8,6 +9,7 @@ export default function ModalProvider() {
             <CreateDeckModal />
             <GenerateQuizModal />
             <RequestPublicDeckModal />
+            <StreakIconContainer />
         </>
     );
 }
