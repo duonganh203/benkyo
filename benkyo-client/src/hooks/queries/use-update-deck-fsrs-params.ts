6af1a325413609567e217a97b917f57a140806a7
duonganh203 <rankill52@gmail.com>
2025-07-06 12:48:50 +0700

    #215: fix type update deck

--- benkyo-client/src/hooks/queries/use-update-deck-fsrs-params.ts
+++ benkyo-client/src/hooks/queries/use-update-deck-fsrs-params.ts
@@ -1,17 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import { updateDeckFsrsParams } from '@/api/deckApi';
+import { FSRSParamsSchema } from '@/schemas/deckSchema';
+import { z } from 'zod';
 
 interface UpdateDeckFsrsParamsInput {
     deckId: string;
-    fsrsParams: {
-        request_retention?: number;
-        maximum_interval?: number;
-        w?: number[];
-        enable_fuzz?: boolean;
-        enable_short_term?: boolean;
-        card_limit?: number;
-        lapses?: number;
-    };
+    fsrsParams: z.infer<typeof FSRSParamsSchema>;
 }
 
 export const useUpdateDeckFsrsParams = () => {
