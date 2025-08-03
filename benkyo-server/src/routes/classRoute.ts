39594c3659a535e51f6bc4249cea46ade6e244eb
Poiseidoncoder <poiseidoncoder@gmail.com>
2025-08-03 18:39:43 +0700

    #327: done refactor-request-join-class-backend

--- benkyo-server/src/routes/classRoute.ts
+++ benkyo-server/src/routes/classRoute.ts
@@ -27,7 +27,7 @@ classRoutes.post('/reject', errorHandler(classController.rejectJoinRequest));
 classRoutes.post('/create', errorHandler(classController.classCreate));
 classRoutes.post('/invite', errorHandler(classController.inviteMemberToClass));
 classRoutes.post('/add-deck', errorHandler(classController.addDeckToClass));
-classRoutes.post('/:_id/request', errorHandler(classController.requestJoinClass));
+classRoutes.post('/:classId/request', errorHandler(classController.classRequestJoin));
 
 classRoutes.put('/:_id/update', errorHandler(classController.updateClass));
 
