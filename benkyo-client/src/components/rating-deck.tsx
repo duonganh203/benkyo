926665e7c3cf8f80ef5a6397e199b0d8d2332c79
thienduyen12930 <144190277+thienduyen12930@users.noreply.github.com>
2025-09-26 23:00:37 +0700

    #369: rating-deck

--- benkyo-client/src/components/rating-deck.tsx
+++ benkyo-client/src/components/rating-deck.tsx
@@ -62,13 +62,15 @@ export default function LikeDeck({ deckData, currentUser, onLikeApi }: LikeDeckP
 
     return (
         <div className='flex items-center gap-2 text-sm'>
-            {isPublic && !isOwner && (
+            {deckData.publicStatus === 2 && currentUser?._id !== deckData.owner._id && (
                 <button
                     type='button'
-                    onClick={handleLike}
-                    disabled={loading}
-                    className={`flex items-center gap-1 px-2.5 py-1 rounded-full border border-border transition-colors shadow-sm
-            ${liked ? 'bg-blue-100 text-blue-600 border-blue-200' : 'bg-muted/60 text-muted-foreground hover:bg-blue-50'}`}
+                    onClick={handleLike} // gọi hàm
+                    className={`flex items-center gap-1 px-2.5 py-1 rounded-full border transition-colors ${
+                        liked
+                            ? 'bg-blue-100 text-blue-600 border-blue-200'
+                            : 'bg-muted/60 text-muted-foreground hover:bg-blue-50'
+                    }`}
                 >
                     <ThumbsUp
                         className={`h-5 w-5 ${liked ? 'fill-blue-500 text-blue-500' : 'text-muted-foreground'}`}
@@ -77,12 +79,10 @@ export default function LikeDeck({ deckData, currentUser, onLikeApi }: LikeDeckP
                 </button>
             )}
 
-            {isPublic && (
-                <div className='flex items-center gap-1 px-2 py-0.5 bg-muted/60 border border-border rounded-full shadow-sm min-w-[50px]'>
-                    <ThumbsUp className='h-4 w-4 text-blue-500 fill-blue-500' />
-                    <span className='font-semibold'>{totalLikes}</span>
-                </div>
-            )}
+            <div className='flex items-center gap-1 px-2 py-0.5 bg-muted/60 border rounded-full min-w-[50px]'>
+                <ThumbsUp className='h-4 w-4 text-blue-500 fill-blue-500' />
+                <span className='font-semibold'>{totalLikes}</span>
+            </div>
         </div>
     );
 }
