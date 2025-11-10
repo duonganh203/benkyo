b5f8cf6abc2db226a7ab654ce10f029f0f3e82d5
Mai Van Thi <poiseidoncoder@gmail.com>
2025-11-10 22:59:23 +0900

    #455: done fix paging

--- benkyo-client/src/hooks/queries/use-get-my-class.ts
+++ benkyo-client/src/hooks/queries/use-get-my-class.ts
@@ -12,10 +12,11 @@ type PaginatedClassList = {
 const useGetMyClass = (search?: string) => {
     return useInfiniteQuery<PaginatedClassList, AxiosError<ApiError>>({
         queryKey: ['myClasses', search],
-        queryFn: ({ pageParam = 1 }) => getMyClassApi(pageParam as number, search),
+        queryFn: ({ pageParam = 1 }) => getMyClassApi(pageParam as number, search, 6),
         initialPageParam: 1,
         getNextPageParam: (lastPage: PaginatedClassList, allPages: PaginatedClassList[]) =>
-            lastPage.hasMore ? allPages.length + 1 : undefined
+            lastPage.hasMore ? allPages.length + 1 : undefined,
+        retry: 1
     });
 };
 
