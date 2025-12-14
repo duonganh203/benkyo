073afb173b5763776cffa4fa1ee27b1e9257ffab
Mai Van Thi <poiseidoncoder@gmail.com>
2025-12-14 18:41:42 +0700

    #558: fix navigate class

--- benkyo-client/src/hooks/queries/use-request-join-class.ts
+++ benkyo-client/src/hooks/queries/use-request-join-class.ts
@@ -4,22 +4,19 @@ import { ApiError } from '@/types/api';
 import { requestJoinClassApi } from '@/api/classApi';
 import { ClassJoinRequestDto, ClassJoinResponseDto } from '@/types/class';
 import { getToast } from '@/utils/getToast';
-import { useNavigate } from 'react-router-dom';
 
 const useRequestJoinClass = () => {
     const queryClient = useQueryClient();
-    const navigate = useNavigate();
     return useMutation<ClassJoinResponseDto, AxiosError<ApiError>, ClassJoinRequestDto>({
         mutationFn: ({ classId }) => requestJoinClassApi(classId),
         onSuccess: (data, variables) => {
-            getToast('success', data.message || 'Requested to join class');
+            getToast('success', data.message);
             queryClient.invalidateQueries({ queryKey: ['myClasses'] });
             queryClient.invalidateQueries({ queryKey: ['suggestedClasses'] });
             queryClient.invalidateQueries({ queryKey: ['class', variables.classId] });
-            navigate(`/class/${variables.classId}`);
         },
         onError: (error) => {
-            getToast('error', error?.message || 'Failed to request join');
+            getToast('error', error?.message);
         }
     });
 };
